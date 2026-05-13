5eec505aacb5a16cb98df1c457e1499cfe821644
Kupaulsh <johnpauldumigpi.rebuyas@gmail.com>
2026-05-13 16:34:46 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Finihomes Admin - Service Worker
+const CACHE = 'finihomes-admin-v1';
+const OFFLINE_URLS = ['/admin.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(OFFLINE_URLS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Only cache same-origin GET requests
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
